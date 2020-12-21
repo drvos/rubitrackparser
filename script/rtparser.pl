51b50a8172fa118efa967477163d3b54837b600c
Volker Schering <mail@volker-schering.de>
2020-12-21 16:25:59 +0100

    Einfaches Script zur Benutzung von rtParser

---
+++ script/rtparser.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+#
+##
+use strict;
+use warnings;
+use v5.018;
+
+use lib './lib';
+
+use rtParser;
+
+my ($file) = @ARGV;
+
+die "Need rubiTrack Exportfile\n" unless defined $file;
+
+my $p = rtParser->new( 'rtexportfile' => $file );
+say $p->as_html();
+
+exit 0;
